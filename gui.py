1d4c37f97c13405e525706fb1d923efb4fc988b2
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2023-03-22 12:02:50 +0530

    Add system to get information on expenses from the date

--- gui.py
+++ gui.py
@@ -3,6 +3,8 @@ This is the GUI version of budget maker
 """
 import tkinter as tk
 from tkinter import ttk
+from tkcalendar import Calendar
+import datetime
 from main import *
 from src.constants import *
 
@@ -70,29 +72,59 @@ class App:
         """
         The function to view previous records
         """
-        subWindow = tk.Toplevel(self.master, height=400)
+        subWindow = tk.Toplevel(self.master)
         subWindow.resizable(False, False)
         subWindow.title("View Records")
 
-        infoPanel = tk.Frame(subWindow, width=150)
-        infoPanel.pack(side='left', expand='yes')
+        infoPanel = tk.Frame(subWindow)
+        infoPanel.pack(side='left', expand='no')
 
-        recordPanel = tk.Frame(subWindow, width=200)
-        recordPanel.pack(side='left', expand='yes')
+        recordPanel = tk.Frame(subWindow, width=400)
+        recordPanel.pack(side='left')
 
         recordListBox = tk.Listbox(recordPanel, selectmode='browse')
-        recordListBox.pack(side='left')
+        recordListBox.pack(side='left', fill='both')
         # Add a slider for the records box
         recordScroll = ttk.Scrollbar(recordPanel)
-        recordScroll.pack(side='left')
+        recordScroll.pack(side='left', fill='y')
 
 
         """ Select a date """
-        tk.Label(infoPanel, text='Select a date', **GUIstyle['label']) \
-        .grid(row=0, column=0, padx=20, pady=20)
+        tk.Label(infoPanel, text='Select a date', **GUIstyle['label-width-small']) \
+        .grid(row=0, column=0, padx=10, pady=10)
+
+        # Fetch today's date
+        today = datetime.datetime.today()
+        # The calendar widget
+        self.calendarWidget = Calendar(infoPanel, selectmode='day', 
+                                  year=today.year, 
+                                  month=today.month, 
+                                  day=today.day)
+        self.calendarWidget.grid(row=1, column=0, padx=10, pady=10)
+        
+        # Button to initiate the process
+        tk.Button(infoPanel, text='Get information', **GUIstyle['button'], command=self.functionViewDateRecord) \
+        .grid(row=2, column=0, padx=10, pady=10)
 
 
+    def functionViewDateRecord(self) -> None:
+        """
+        Fetches today's date and show the expenses for the day in listbox
+        """
+        # Fetch the date from the calendar
+        date = datetime.datetime.strptime(
+            self.calendarWidget.get_date(),
+            "%m/%d/%y"
+        )
+        # Change the format of the date to match the one in database
+        database_formatted_date = datetime.datetime.strftime(date, "%b-%d-%Y")
+        print(database_formatted_date)
 
+        # Request the data for this date from the database
+        date_records = db_handler.records_with_date(database_formatted_date)
+
+        print(date_records)
+        
 
 
 if __name__ == '__main__':
