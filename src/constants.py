1edd1f12fe605968b5498fcfd75b657b2fec4eec
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2023-03-22 12:02:04 +0530

    Add more style properties. Add new tuple to hold type of expenses

--- src/constants.py
+++ src/constants.py
@@ -5,15 +5,27 @@ APP_TITLE = "Money-Manager"
 
 RESIZABLE = (False, False)
 
+EXPENSE_CATEGORIES = (
+    "Transportation",
+    "Snacks, eating out, and food in general",
+    "School Supplies",
+    "Movies, music and entertainment",
+    "Hobby",
+    "Cell phone or internet",
+    "Savings"
+)
+
 GUIstyle = {
     'button': {
         'relief': 'groove',
-        'width': 40
+        'width': 30
     },
     'label': {
         'relief': 'groove',
         'borderwidth': 2,
         'font': 'Verdana 12',
-        'width': 40
+        'width': 30
     }
 }
+GUIstyle['label-width-small'] = GUIstyle['label']
+GUIstyle['label-width-small']['width'] = 20
